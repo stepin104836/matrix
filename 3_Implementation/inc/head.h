23e881c8c8542a28c02f673b2692775600b4a467
stepin104836 <69116391+stepin104836@users.noreply.github.com>
2020-09-19 00:53:04 +0530

    Add files via upload

---
+++ 3_Implementation/inc/head.h
@@ -0,0 +1,12 @@
+
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+void add(int matrix_a[3][3], int matrix_b[3][3]);
+void mul(int matrix_a[3][3], int matrix_b[3][3]);
+#endif
