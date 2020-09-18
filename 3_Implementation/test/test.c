d325e6e0430017ea358587c5835ce8f254db3050
stepin104836 <69116391+stepin104836@users.noreply.github.com>
2020-09-19 00:57:03 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,67 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "Matrix calculator"
+
+
+
+/* Prototypes for all the test functions */
+void add(int_matrix_a[3][3], int matrix_b[3][3]);
+void mul(int matrix_a[3][3], int matrix_b[3][3]);
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  
+  
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "ADDITION", test_add);
+  CU_add_test(suite, "MULTIPLY", test_multiply);
+   
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+void test_add(void) {
+	
+	matrix_a[1][1]={100};
+	matrix_b[1][1]={200};
+	
+	  CU_ASSERT(300 == add(matrix_a[1][1], matrix_b[1][1]);
+  
+  /* Dummy fail*/
+  CU_ASSERT(600 == add(matrix_a[1][1], matrix_b[1][1]);
+}
+
+void test_mul(void) {
+	matrix_a[1][1]={10};
+	matrix_b[1][1]={20};
+	
+  CU_ASSERT(200 == mul(matrix_a[1][1], matrix_b[1][1]);
+  
+  /* Dummy fail*/
+  CU_ASSERT(500 == mul(matrix_a[1][1], matrix_b[1][1]);
+}
+
+
