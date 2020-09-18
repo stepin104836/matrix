dde40ca56d39a6335206a65e0929e38c4910f9f6
stepin104836 <69116391+stepin104836@users.noreply.github.com>
2020-09-19 00:58:51 +0530

    Update and rename project_main.c to main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,56 @@
+int main()
+{
+    
+    int matrix_a[3][3];
+	int matrix_b[3][3];
+	unsigned int calculator_operation=0;
+      
+	  int a;
+	  int b;
+
+    //take first matrix
+    for(a=0;a<3;a++)
+    {
+        for(b=0;b<3;b++)
+        {
+          
+            scanf("%d",&matrix_a[a][b]);
+        }
+    }
+	
+    printf("\n");
+
+    //take second matrix
+    for(a=0;a<3;a++)
+    {
+        for(b=0;b<3;b++)
+        {
+        
+            scanf("%d",&matrix_b[a][b]);
+        }
+    }
+	
+		
+	     scanf("%d", &calculator_operation);
+	
+		
+	switch(calculator_operation)
+    {
+        case 1:
+            printf("Addition");
+            add(matrix_a, matrix_b);
+            break;
+        
+        case 2:
+            printf("Multiplication");
+              mul(matrix_a, matrix_b);
+            break;
+			
+        default:
+            printf("\n\t---It should never come here---\n");
+    }
+	    
+   	//matrix_a and matrix_b passed
+
+    return 0;
+}
